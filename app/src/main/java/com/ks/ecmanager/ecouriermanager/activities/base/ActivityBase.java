ed3983088b91855c4df20d27db252640ffcb7e8a
kazisrabon <kaziiit@gmail.com>
2017-05-22 18:29:57 +0600

    profile update and upload photo

--- app/src/main/java/com/ks/ecmanager/ecouriermanager/activities/base/ActivityBase.java
+++ app/src/main/java/com/ks/ecmanager/ecouriermanager/activities/base/ActivityBase.java
@@ -7,7 +7,15 @@ package com.ks.ecmanager.ecouriermanager.activities.base;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.drawable.Drawable;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
@@ -19,6 +27,8 @@ import android.widget.Toast;
 
 import com.ks.ecmanager.ecouriermanager.R;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 
 /**
@@ -27,6 +37,7 @@ import java.util.HashMap;
 
 public class ActivityBase extends AppCompatActivity {
 
+    public static final String ALBUM_NAME = "eCourier";
     public static final int MIDDLE = 2;
     public static final int START = 1;
     public static final int END = 3;
@@ -36,6 +47,8 @@ public class ActivityBase extends AppCompatActivity {
             REQUEST_EXTERNAL_STORAGE = 401,
             REQUEST_READ_EXTERNAL_STORAGE = 402,
             calender_type = 110;
+    public static Drawable IMG_DRAWABLE = null;
+    public static Bitmap mBitmap = null;
     private final String TAG = "Base";
     private Activity activity;
     private Context context;
@@ -132,5 +145,75 @@ public class ActivityBase extends AppCompatActivity {
         else isValid = false;
 
         return isValid;
+    }
+
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float)width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
+
+    /* Get the real path from the URI */
+    public String getPathFromURI(Uri contentUri) {
+        String res = "";
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getApplicationContext().getContentResolver().query(contentUri, proj, null, null, null);
+        assert cursor != null;
+        cursor.moveToFirst();
+        int column_index = cursor.getColumnIndex(proj[0]);
+        res = cursor.getString(column_index);
+        cursor.close();
+        Log.e(TAG, "Gallery File Path=====>>>"+contentUri);
+        return res;
+    }
+
+    public long getFolderSize(File f) {
+        long size = 0;
+        if (f.isDirectory()) {
+            for (File file : f.listFiles()) {
+                size += getFolderSize(file);
+            }
+        } else {
+            size=f.length();
+        }
+        return size;
+    }
+
+    public Bitmap rotateBitmapOrientation(String photoFilePath) {
+
+        // Create and configure BitmapFactory
+        BitmapFactory.Options bounds = new BitmapFactory.Options();
+        bounds.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(photoFilePath, bounds);
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        Bitmap bm = BitmapFactory.decodeFile(photoFilePath, opts);
+        // Read EXIF Data
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(photoFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
+        int orientation = orientString != null ? Integer.parseInt(orientString) : ExifInterface.ORIENTATION_NORMAL;
+        int rotationAngle = 0;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
+        // Rotate Bitmap
+        Matrix matrix = new Matrix();
+        matrix.setRotate(rotationAngle, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
+        // Return result
+        return rotatedBitmap;
     }
 }
--- app/src/main/java/com/ks/ecmanager/ecouriermanager/activities/firstLayer/ActivityProfile.java
+++ app/src/main/java/com/ks/ecmanager/ecouriermanager/activities/firstLayer/ActivityProfile.java
@@ -10,14 +10,23 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.nfc.Tag;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,9 +38,19 @@ import com.ks.ecmanager.ecouriermanager.session.SessionUserData;
 import com.ks.ecmanager.ecouriermanager.webservices.ApiParams;
 import com.ks.ecmanager.ecouriermanager.webservices.interfaces.ConsignmentListInterface;
 import com.ks.ecmanager.ecouriermanager.webservices.interfaces.ProfileListInterface;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.MultipartBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
 import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -50,9 +69,11 @@ public class ActivityProfile extends ActivityBase {
     private CircleImageView imgProfilePic;
     private ProgressDialog mProgressDialog;
     private FloatingActionButton btnSelectImage;
+    private Button btnTakePhoto, btnOpenGallery;
     private AlertDialog b;
     private File mPhoto;
     private String path = "";
+    private ArrayList<String> image_list=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,19 +96,19 @@ public class ActivityProfile extends ActivityBase {
         txtDOB = (TextView) findViewById(R.id.txtDOB);
         txtNID = (TextView) findViewById(R.id.txtNID);
         imgProfilePic = (CircleImageView) findViewById(R.id.user_profile_photo);
-//        imgProfilePic.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                promptEditPP();
-//            }
-//        });
+        imgProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptEditPP();
+            }
+        });
         btnSelectImage = (FloatingActionButton) findViewById(R.id.btnSelectImage);
-//        btnSelectImage.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                promptEditPP();
-//            }
-//        });
+        btnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptEditPP();
+            }
+        });
     }
 
     private void getProfileData() {
@@ -166,6 +187,169 @@ public class ActivityProfile extends ActivityBase {
 
         if (checkText(profileList.getData().getNid()))
             txtNID.setText("*****************");
+    }
+
+    private void promptEditPP() {
+        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(ActivityProfile.this);
+        LayoutInflater inflater = ActivityProfile.this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.dialog_profile_pic, null);
+        dialogBuilder.setView(dialogView);
+        b = dialogBuilder.create();
+        b.setCancelable(true);
+        b.show();
+
+        btnTakePhoto = (Button) dialogView.findViewById(R.id.btnTakePhoto);
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b.cancel();
+                takePhoto();
+            }
+        });
+        btnOpenGallery = (Button) dialogView.findViewById(R.id.btnOpenGallery);
+        btnOpenGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b.cancel();
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Gallery"), SELECT_PICTURE);
+            }
+        });
+    }
+
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File folder = new File(Environment.getExternalStorageDirectory() + "/"+ALBUM_NAME);
+
+        if(!folder.exists())
+        {
+            folder.mkdir();
+        }
+        final Calendar c = Calendar.getInstance();
+        path = String.format(Environment.getExternalStorageDirectory() +"/"+ALBUM_NAME+"_%d.jpg", System.currentTimeMillis());
+        mPhoto = new File(path);
+        Log.e("URI", Uri.fromFile(mPhoto).toString());
+        Uri uri = FileProvider.getUriForFile(
+                this,
+                getApplicationContext().getPackageName()+".provider",
+                mPhoto);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivityForResult(intent, TAKE_PHOTO);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // Get the path from the Uri
+                    String path = getPathFromURI(selectedImageUri);
+                    mPhoto = new File(path);
+                    Log.e(TAG, "Image file name : " + mPhoto.getName());
+                    // Set the image in ImageView
+                    try {
+                        mBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                        imgProfilePic.setImageBitmap(mBitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (mBitmap == null)
+                        imgProfilePic.setImageURI(selectedImageUri);
+                    Log.e(TAG, "Image file name : " + mPhoto.getName());
+                    sendFile(bitmapToFile(getResizedBitmap(mBitmap, 50)));
+//                    sendFile(mPhoto);
+                }
+            }
+            if(requestCode == TAKE_PHOTO)
+            {
+                image_list.add(path);
+                new GetImages().execute();
+            }
+        }
+    }
+
+    public File bitmapToFile(Bitmap bitmap){
+        try {
+            mPhoto.createNewFile();
+
+            //Convert bitmap to byte array
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 0 /*ignored for PNG*/, bos);
+            byte[] bitmapdata = bos.toByteArray();
+
+            //write the bytes in file
+            FileOutputStream fos = new FileOutputStream(mPhoto);
+            fos.write(bitmapdata);
+            fos.flush();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return mPhoto;
+    }
+
+    private void sendFile(File file){
+        long size = getFolderSize(file)/1024;
+        Log.e("File Size ", ""+size);
+        String id = user.get(SessionUserData.KEY_USER_ID);
+        String group = user.get(SessionUserData.KEY_USER_GROUP);
+        String authentication_key = user.get(SessionUserData.KEY_USER_AUTH_KEY);
+
+        //Lets pass the desired parameters
+        HashMap<String, String> map = new HashMap<String, String>();
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = new MultipartBuilder()
+                .type(MultipartBuilder.FORM)
+                .addFormDataPart(ApiParams.PARAM_AUTHENTICATION_KEY, authentication_key)
+                .addFormDataPart(ApiParams.PARAM_ADMIN_ID, id)
+                .addFormDataPart(ApiParams.PARAM_GROUP, group)
+                .addFormDataPart("filename", file.getName(),
+                        RequestBody.create(MediaType.parse("jpg"), file))
+                .build();
+        Log.e(TAG, id+" "+group+" "+authentication_key+" "+file.getName());
+        String serverURL = "";
+        serverURL = ApiParams.TAG_BASE_URL + "/"+ApiParams.TAG_PROFILE_UPDATE_KEY;
+        Log.e("Server Url", serverURL);
+        Request request = new Request.Builder()
+                .url(serverURL)
+                .post(requestBody)
+                .build();
+        showProgressDialog(false, "", "Sending...");
+        client.newCall(request).enqueue(new com.squareup.okhttp.Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.e(TAG, "Failed");
+                hideProgressDialog();
+            }
+            @Override
+            public void onResponse(com.squareup.okhttp.Response response) throws IOException {
+                Log.e(TAG, response.message());
+                hideProgressDialog();
+            }
+        });
+    }
+
+    private class GetImages extends AsyncTask<Void, Void, Bitmap>
+    {
+        @Override
+        protected Bitmap doInBackground(Void... params)
+        {
+            for(int i=0; i<image_list.size(); i++)
+            {
+                mBitmap = rotateBitmapOrientation(path);
+                return mBitmap;
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Bitmap bitmap)
+        {
+            imgProfilePic.setImageBitmap(bitmap);
+            sendFile(bitmapToFile(getResizedBitmap(bitmap, 50)));
+        }
     }
 
     @Override
